e8061763ad3cb588e3b41dbe683888bc823e81b0
Nikita <nsalogub96@gmail.com>
2023-12-27 12:51:50 +0200

    done

--- src/main/java/mate/academy/EventManager.java
+++ src/main/java/mate/academy/EventManager.java
@@ -1,19 +1,38 @@
 package mate.academy;
 
-public class EventManager {
-    public void registerListener(EventListener listener) {
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingDeque;
 
+public class EventManager {
+    private final BlockingDeque<EventListener> queue;
+    private final ExecutorService executorService;
+
+    public EventManager() {
+        this.queue = new LinkedBlockingDeque<>();
+        this.executorService = Executors.newCachedThreadPool();
+    }
+
+    public void registerListener(EventListener listener) {
+        queue.add(listener);
     }
 
     public void deregisterListener(EventListener listener) {
-
+        queue.removeFirstOccurrence(listener);
     }
 
     public void notifyEvent(Event event) {
-
+        queue.forEach(listener ->
+                executorService.submit(() ->
+                        listener.onEvent(event)
+                )
+        );
     }
 
     public void shutdown() {
-
+        queue.forEach(listener ->
+                executorService.shutdown());
+        queue.clear();
     }
 }
