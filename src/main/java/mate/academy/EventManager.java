83e3efde0621cf8c2dcf69cd5d5534800c7a9bc7
Rostyslav Kuzmych <rostislavkuzmic401@gmail.com>
2024-01-02 16:37:06 +0200

    added notification asynchronously

--- src/main/java/mate/academy/EventManager.java
+++ src/main/java/mate/academy/EventManager.java
@@ -2,9 +2,12 @@ package mate.academy;
 
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class EventManager {
     private List<EventListener> listeners = new CopyOnWriteArrayList<>();
+    private ExecutorService executorService = Executors.newFixedThreadPool(10);
 
     public void registerListener(EventListener listener) {
         listeners.add(listener);
@@ -16,11 +19,12 @@ public class EventManager {
 
     public void notifyEvent(Event event) {
         for (EventListener eventListener : listeners) {
-            eventListener.onEvent(event);
+            executorService.execute(() -> eventListener.onEvent(event));
         }
     }
 
     public void shutdown() {
+        executorService.shutdown();
         listeners.clear();
     }
 }
