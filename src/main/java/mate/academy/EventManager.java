9136ff6275092ab6418e86abb1ce00b0c5554bcb
denysSmeiNewBom <den.smei.ds@gmail.com>
2024-01-05 13:06:51 +0200

    Refactoring solution with ExecutorService

--- src/main/java/mate/academy/EventManager.java
+++ src/main/java/mate/academy/EventManager.java
@@ -2,9 +2,12 @@ package mate.academy;
 
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class EventManager {
     private List<EventListener> listenersQueue = new CopyOnWriteArrayList<>();
+    private final ExecutorService executorService = Executors.newCachedThreadPool();
 
     public void registerListener(EventListener listener) {
         listenersQueue.add(listener);
@@ -15,10 +18,12 @@ public class EventManager {
     }
 
     public void notifyEvent(Event event) {
-        listenersQueue.forEach(listener -> listener.onEvent(event));
+        listenersQueue.forEach(listener -> executorService.submit(
+                () -> listener.onEvent(event)));
     }
 
     public void shutdown() {
-
+        listenersQueue.clear();
+        executorService.shutdown();
     }
 }
