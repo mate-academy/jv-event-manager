9a240cddf492a446adb53c7312eb9ea78f2478d7
denysSmeiNewBom <den.smei.ds@gmail.com>
2024-01-05 13:02:20 +0200

    Task completed

--- src/main/java/mate/academy/EventManager.java
+++ src/main/java/mate/academy/EventManager.java
@@ -1,16 +1,21 @@
 package mate.academy;
 
-public class EventManager {
-    public void registerListener(EventListener listener) {
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
+public class EventManager {
+    private List<EventListener> listenersQueue = new CopyOnWriteArrayList<>();
+
+    public void registerListener(EventListener listener) {
+        listenersQueue.add(listener);
     }
 
     public void deregisterListener(EventListener listener) {
-
+        listenersQueue.remove(listener);
     }
 
     public void notifyEvent(Event event) {
-
+        listenersQueue.forEach(listener -> listener.onEvent(event));
     }
 
     public void shutdown() {
