11bdb3749c150b1acbb95c4dd90c24b4206bc2f0
Nikita Ostrenskyi <nikostrik2003@gmail.com>
2024-01-22 14:23:50 +0200

    implemented event manager

--- src/main/java/mate/academy/EventManager.java
+++ src/main/java/mate/academy/EventManager.java
@@ -1,19 +1,27 @@
 package mate.academy;
 
-public class EventManager {
-    public void registerListener(EventListener listener) {
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
+public class EventManager {
+    private final ConcurrentLinkedQueue<EventListener> eventListeners =
+            new ConcurrentLinkedQueue<>();
+
+    public void registerListener(EventListener listener) {
+        eventListeners.offer(listener);
     }
 
     public void deregisterListener(EventListener listener) {
-
+        eventListeners.remove(listener);
     }
 
     public void notifyEvent(Event event) {
-
+        for (EventListener eventListener : eventListeners) {
+            CompletableFuture.runAsync(() -> eventListener.onEvent(event));
+        }
     }
 
     public void shutdown() {
-
+        eventListeners.clear();
     }
 }
