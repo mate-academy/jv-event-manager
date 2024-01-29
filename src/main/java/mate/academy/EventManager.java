7ef39bc97bb4e8f86bc9539e62a153adffc58042
Igor Korolevich <igorkorolj@gmail.com>
2024-01-29 18:59:52 +0200

    implemented methods

--- src/main/java/mate/academy/EventManager.java
+++ src/main/java/mate/academy/EventManager.java
@@ -1,19 +1,26 @@
 package mate.academy;
 
-public class EventManager {
-    public void registerListener(EventListener listener) {
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
+public class EventManager {
+    private List<EventListener> eventListenerList = new CopyOnWriteArrayList<>();
+
+    public void registerListener(EventListener listener) {
+        eventListenerList.add(listener);
     }
 
     public void deregisterListener(EventListener listener) {
-
+        eventListenerList.remove(listener);
     }
 
     public void notifyEvent(Event event) {
-
+        for (EventListener eventListener : eventListenerList) {
+            eventListener.onEvent(event);
+        }
     }
 
     public void shutdown() {
-
+        eventListenerList.clear();
     }
 }
