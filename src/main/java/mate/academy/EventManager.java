604ee2408676c87bc2b093715209b9eb985c8e54
Lenovo <liubchenkalexander@gmail.com>
2024-01-30 01:31:56 +0200

    done the task

--- src/main/java/mate/academy/EventManager.java
+++ src/main/java/mate/academy/EventManager.java
@@ -1,19 +1,29 @@
 package mate.academy;
 
-public class EventManager {
-    public void registerListener(EventListener listener) {
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
+public class EventManager {
+    private final List<EventListener> eventListenerList = new CopyOnWriteArrayList<>();
+    private final ExecutorService executorService = Executors.newFixedThreadPool(4);
+
+    public void registerListener(EventListener listener) {
+        eventListenerList.add(listener);
     }
 
     public void deregisterListener(EventListener listener) {
-
+        eventListenerList.remove(listener);
     }
 
     public void notifyEvent(Event event) {
-
+        eventListenerList.forEach(eventListener -> CompletableFuture
+                .runAsync(() -> eventListener.onEvent(event), executorService));
     }
 
     public void shutdown() {
-
+        executorService.shutdown();
     }
 }
