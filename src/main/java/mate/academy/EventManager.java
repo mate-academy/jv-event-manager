2c879edf59a1ba079cafb57f9d1973d733661099
Bohdan Bilko <rapsbodik@gmail.com>
2024-01-30 11:38:30 +0200

    added solution

--- src/main/java/mate/academy/EventManager.java
+++ src/main/java/mate/academy/EventManager.java
@@ -1,19 +1,29 @@
 package mate.academy;
 
-public class EventManager {
-    public void registerListener(EventListener listener) {
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
+public class EventManager {
+    private static final Integer NUMBER_OF_THREADS = 4;
+    private final List<EventListener> listeners = new CopyOnWriteArrayList<>();
+    private final ExecutorService executorService = Executors.newFixedThreadPool(NUMBER_OF_THREADS);
+
+    public void registerListener(EventListener listener) {
+        listeners.add(listener);
     }
 
     public void deregisterListener(EventListener listener) {
-
+        listeners.remove(listener);
     }
 
     public void notifyEvent(Event event) {
-
+        listeners.forEach(l -> executorService.submit(() -> l.onEvent(event)));
     }
 
     public void shutdown() {
-
+        listeners.clear();
+        executorService.shutdown();
     }
 }
