41c716dd073538e8f79f18c58f447120b9724b9b
Nataliia <gzodchie@gmail.com>
2024-01-30 15:01:49 +0200

    added event manager implementation

--- src/main/java/mate/academy/EventManager.java
+++ src/main/java/mate/academy/EventManager.java
@@ -1,19 +1,34 @@
 package mate.academy;
 
-public class EventManager {
-    public void registerListener(EventListener listener) {
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
+public class EventManager {
+    private final List<EventListener> listeners;
+    private final ExecutorService executorService;
+
+    public EventManager() {
+        this.listeners = new CopyOnWriteArrayList<>();
+        this.executorService = Executors.newCachedThreadPool();
+    }
+
+    public void registerListener(EventListener listener) {
+        listeners.add(listener);
     }
 
     public void deregisterListener(EventListener listener) {
-
+        listeners.remove(listener);
     }
 
     public void notifyEvent(Event event) {
-
+        for (EventListener listener : listeners) {
+            executorService.submit(() -> listener.onEvent(event));
+        }
     }
 
     public void shutdown() {
-
+        executorService.shutdown();
     }
 }
