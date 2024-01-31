3ae1ad29b0c7b95edc8a7e0649b99106c1b3a8af
Yura Uniyat <yurauniyat123@gmail.com>
2024-01-31 14:18:31 +0200

    Homework java-mt-implement a thread-safe asynchronous event manager

--- src/main/java/mate/academy/EventManager.java
+++ src/main/java/mate/academy/EventManager.java
@@ -1,19 +1,40 @@
 package mate.academy;
 
-public class EventManager {
-    public void registerListener(EventListener listener) {
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
+public class EventManager {
+    private final Map<EventListener, ExecutorService> listenerExecutors = new ConcurrentHashMap<>();
+
+    public void registerListener(EventListener listener) {
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        listenerExecutors.put(listener, executorService);
     }
 
     public void deregisterListener(EventListener listener) {
-
+        ExecutorService executorService = listenerExecutors.remove(listener);
+        if (executorService != null) {
+            executorService.shutdown();
+        }
     }
 
     public void notifyEvent(Event event) {
-
+        Set<Map.Entry<EventListener, ExecutorService>> entries = listenerExecutors.entrySet();
+        for (Map.Entry<EventListener, ExecutorService> entry : entries) {
+            ExecutorService executorService = entry.getValue();
+            EventListener listener = entry.getKey();
+            executorService.submit(() -> {
+                listener.onEvent(event);
+                listener.getProcessedEvent();
+            });
+        }
     }
 
     public void shutdown() {
-
+        listenerExecutors.values().forEach(ExecutorService::shutdown);
+        listenerExecutors.clear();
     }
 }
