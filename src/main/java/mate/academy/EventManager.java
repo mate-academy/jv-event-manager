3570eca90e3afbf406222b2829b2dafcd932d941
Vlad Bondarev <bondarev.21@icloud.com>
2024-04-12 11:06:42 +0300

    hw

--- src/main/java/mate/academy/EventManager.java
+++ src/main/java/mate/academy/EventManager.java
@@ -1,19 +1,28 @@
 package mate.academy;
 
-public class EventManager {
-    public void registerListener(EventListener listener) {
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
+public class EventManager {
+    private final List<EventListener> list;
+
+    public EventManager() {
+        list = new CopyOnWriteArrayList<>();
+    }
+
+    public void registerListener(EventListener listener) {
+        list.add(listener);
     }
 
     public void deregisterListener(EventListener listener) {
-
+        list.remove(listener);
     }
 
     public void notifyEvent(Event event) {
-
+        list.forEach(listener -> listener.onEvent(event));
     }
 
     public void shutdown() {
-
+        list.clear();
     }
 }
