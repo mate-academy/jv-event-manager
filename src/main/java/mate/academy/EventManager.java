1238d50eccbe5d9e6911a828981a527afe2e540a
Krasnov-Maksim <maksim.v.krasnov@gmail.com>
2024-04-22 17:39:49 +0300

    Solved jv-event-manager

--- src/main/java/mate/academy/EventManager.java
+++ src/main/java/mate/academy/EventManager.java
@@ -1,19 +1,32 @@
 package mate.academy;
 
-public class EventManager {
-    public void registerListener(EventListener listener) {
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
+public class EventManager {
+    private final ConcurrentLinkedQueue<EventListener> listeners = new ConcurrentLinkedQueue<>();
+    private final ExecutorService executorService;
+
+    public EventManager() {
+        int cores = Runtime.getRuntime().availableProcessors();
+        this.executorService = Executors.newFixedThreadPool(cores == 1 ? 1 : cores - 1);
+    }
+
+    public void registerListener(EventListener listener) {
+        listeners.offer(listener);
     }
 
     public void deregisterListener(EventListener listener) {
-
+        listeners.remove(listener);
     }
 
     public void notifyEvent(Event event) {
-
+        listeners.forEach(listener -> executorService.submit(() -> listener.onEvent(event)));
     }
 
     public void shutdown() {
-
+        listeners.clear();
+        executorService.shutdown();
     }
 }
