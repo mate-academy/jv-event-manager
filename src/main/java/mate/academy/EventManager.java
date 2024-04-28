204358310c2ef70d808b76e9724705e07bfac7df
Olexandr Bastov <iamlucky1990@gmail.com>
2024-04-29 02:09:06 +0300

    task complete

--- src/main/java/mate/academy/EventManager.java
+++ src/main/java/mate/academy/EventManager.java
@@ -1,15 +1,27 @@
 package mate.academy;
 
-public class EventManager {
-    public void registerListener(EventListener listener) {
+import java.util.concurrent.CopyOnWriteArrayList;
 
+public class EventManager {
+    private final CopyOnWriteArrayList<EventListener> listeners;
+
+    public EventManager() {
+        this.listeners = new CopyOnWriteArrayList<>();
+    }
+
+    public void registerListener(EventListener listener) {
+        listeners.add(listener);
     }
 
     public void deregisterListener(EventListener listener) {
-
+        listeners.remove(listener);
     }
 
     public void notifyEvent(Event event) {
+        for (EventListener listener : listeners) {
+            Thread thread = new Thread(() -> listener.onEvent(event));
+            thread.start();
+        }
 
     }
 
