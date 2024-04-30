8cf897138be6030b8c1156a66d6c6d6f46df2e8f
Ruslan Cherniavskyi <ruslan.od.ua@ukr.net>
2024-04-30 13:45:27 +0300

    add EventManager

--- src/main/java/mate/academy/EventManager.java
+++ src/main/java/mate/academy/EventManager.java
@@ -1,19 +1,34 @@
 package mate.academy;
 
-public class EventManager {
-    public void registerListener(EventListener listener) {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
+public class EventManager {
+    private final List<EventListener> listeners;
+    private final ExecutorService executorService;
+
+    public EventManager() {
+        this.listeners = new ArrayList<>();
+        this.executorService = Executors.newCachedThreadPool();
+    }
+
+    public void registerListener(EventListener listener) {
+        listeners.add(listener);
     }
 
     public void deregisterListener(EventListener listener) {
-
+        listeners.remove(listener);
     }
 
     public void notifyEvent(Event event) {
-
+        for (EventListener listener : listeners) {
+            executorService.execute(() -> listener.onEvent(event));
+        }
     }
 
     public void shutdown() {
-
+        executorService.shutdown();
     }
 }
