5791bc2316975c73cda54d5a4aa10c7691c2d602
JadeRowan <rougesoul.m@gmail.com>
2024-05-03 21:31:48 +0300

    So what collection i was supposed to use and why?

--- src/main/java/mate/academy/EventManager.java
+++ src/main/java/mate/academy/EventManager.java
@@ -1,16 +1,25 @@
 package mate.academy;
 
-public class EventManager {
-    public void registerListener(EventListener listener) {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
+public class EventManager {
+
+    private final List<EventListener> managedListeners = new ArrayList<>();
+
+    public void registerListener(EventListener listener) {
+        managedListeners.add(listener);
     }
 
     public void deregisterListener(EventListener listener) {
-
+        managedListeners.remove(listener);
     }
 
     public void notifyEvent(Event event) {
-
+        managedListeners.forEach(
+                listener -> CompletableFuture.runAsync(() -> listener.onEvent(event))
+        );
     }
 
     public void shutdown() {
