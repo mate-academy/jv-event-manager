6858e2b59e7f54dee35516f792fc64715e047795
Yaroslav Radevych-Vynnytskyi <loghitechdf@gmail.com>
2024-05-12 17:46:22 +0300

    implemented event manager

--- src/main/java/mate/academy/EventManager.java
+++ src/main/java/mate/academy/EventManager.java
@@ -1,19 +1,29 @@
 package mate.academy;
 
-public class EventManager {
-    public void registerListener(EventListener listener) {
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
+public class EventManager {
+    private final List<EventListener> data;
+
+    public EventManager() {
+        data = new CopyOnWriteArrayList<>();
+    }
+
+    public void registerListener(EventListener listener) {
+        data.add(listener);
     }
 
     public void deregisterListener(EventListener listener) {
-
+        data.remove(listener);
     }
 
     public void notifyEvent(Event event) {
-
+        for (EventListener datum : data) {
+            datum.onEvent(event);
+        }
     }
 
     public void shutdown() {
-
     }
 }
