7d161ead06afdceb51a50d3094916c369ca0a848
Evgeniy Chernonog <chernonog.evgeniy@gmail.com>
2024-05-19 20:41:19 +0300

    added hw solution

--- src/main/java/mate/academy/EventManager.java
+++ src/main/java/mate/academy/EventManager.java
@@ -1,19 +1,40 @@
 package mate.academy;
 
-public class EventManager {
-    public void registerListener(EventListener listener) {
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
+public class EventManager {
+    private static final int AWAIT_TERMINATION_TIMEOUT = 60;
+    private static final TimeUnit TIME_UNIT = TimeUnit.SECONDS;
+
+    private final ConcurrentHashMap<EventListener, Boolean> listeners = new ConcurrentHashMap<>();
+    private final ExecutorService executor = Executors.newCachedThreadPool();
+
+    public void registerListener(EventListener listener) {
+        listeners.put(listener, true);
     }
 
     public void deregisterListener(EventListener listener) {
-
+        listeners.remove(listener);
     }
 
     public void notifyEvent(Event event) {
-
+        for (EventListener listener : listeners.keySet()) {
+            executor.submit(() -> listener.onEvent(event));
+        }
     }
 
     public void shutdown() {
-
+        executor.shutdown();
+        try {
+            if (!executor.awaitTermination(AWAIT_TERMINATION_TIMEOUT, TIME_UNIT)) {
+                executor.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            executor.shutdownNow();
+            Thread.currentThread().interrupt();
+        }
     }
 }
