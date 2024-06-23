b3ffce4e4eea091ba1e3181cd432b2ae13cdf27f
Anton Uzhva <anton.itec91@gmail.com>
2024-06-23 15:47:33 +0300

    Completed task

--- src/main/java/mate/academy/EventManager.java
+++ src/main/java/mate/academy/EventManager.java
@@ -1,19 +1,28 @@
 package mate.academy;
 
-public class EventManager {
-    public void registerListener(EventListener listener) {
+import java.util.concurrent.ConcurrentLinkedQueue;
 
+public class EventManager {
+    private ConcurrentLinkedQueue<EventListener> queueu;
+
+    public EventManager() {
+        this.queueu = new ConcurrentLinkedQueue<>();
+    }
+
+    public void registerListener(EventListener listener) {
+        queueu.offer(listener);
     }
 
     public void deregisterListener(EventListener listener) {
-
+        queueu.remove(listener);
     }
 
     public void notifyEvent(Event event) {
+        queueu.forEach(eventListener -> eventListener.onEvent(event));
 
     }
 
     public void shutdown() {
-
+        queueu.clear();
     }
 }
