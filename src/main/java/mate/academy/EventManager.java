559b8bc3d6a318d7b631ff91ef79d7053fabf7db
MolchanovAlexander <rybkolhoz@gmail.com>
2024-06-24 18:35:40 +0300

    improved solution

--- src/main/java/mate/academy/EventManager.java
+++ src/main/java/mate/academy/EventManager.java
@@ -1,25 +1,40 @@
 package mate.academy;
 
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class EventManager {
-    private final CopyOnWriteArrayList<EventListener> queue = new CopyOnWriteArrayList<>();
+    private final CopyOnWriteArrayList<EventListener> list = new CopyOnWriteArrayList<>();
+    private final ExecutorService executor = Executors.newFixedThreadPool(5);
 
     public void registerListener(EventListener listener) {
-        queue.add(listener);
+        list.add(listener);
     }
 
     public void deregisterListener(EventListener listener) {
-        queue.remove(listener);
+        list.remove(listener);
     }
 
     public void notifyEvent(Event event) {
-        for (EventListener listener : queue) {
-            listener.onEvent(event);
+        for (EventListener listener : list) {
+            executor.submit(() -> listener.onEvent(event), executor);
         }
     }
 
     public void shutdown() {
-        System.out.println("shutdown");
+        executor.shutdown();
+        try {
+            if (!executor.awaitTermination(60, TimeUnit.MILLISECONDS)) {
+                executor.shutdownNow();
+                if (!executor.awaitTermination(60, TimeUnit.MILLISECONDS)) {
+                    System.err.println("ExecutorService did not terminate");
+                }
+            }
+        } catch (InterruptedException ie) {
+            executor.shutdownNow();
+            Thread.currentThread().interrupt();
+        }
     }
 }
