b90c65cfeb1a757a708efe75a5be28ff542287b7
MolchanovAlexander <rybkolhoz@gmail.com>
2024-06-27 09:42:25 +0300

    list renamed and redundant removed

--- src/main/java/mate/academy/EventManager.java
+++ src/main/java/mate/academy/EventManager.java
@@ -3,38 +3,26 @@ package mate.academy;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 
 public class EventManager {
-    private final CopyOnWriteArrayList<EventListener> list = new CopyOnWriteArrayList<>();
+    private final CopyOnWriteArrayList<EventListener> listeners = new CopyOnWriteArrayList<>();
     private final ExecutorService executor = Executors.newFixedThreadPool(5);
 
     public void registerListener(EventListener listener) {
-        list.add(listener);
+        listeners.add(listener);
     }
 
     public void deregisterListener(EventListener listener) {
-        list.remove(listener);
+        listeners.remove(listener);
     }
 
     public void notifyEvent(Event event) {
-        for (EventListener listener : list) {
+        for (EventListener listener : listeners) {
             executor.submit(() -> listener.onEvent(event), executor);
         }
     }
 
     public void shutdown() {
         executor.shutdown();
-        try {
-            if (!executor.awaitTermination(60, TimeUnit.MILLISECONDS)) {
-                executor.shutdownNow();
-                if (!executor.awaitTermination(60, TimeUnit.MILLISECONDS)) {
-                    System.err.println("ExecutorService did not terminate");
-                }
-            }
-        } catch (InterruptedException ie) {
-            executor.shutdownNow();
-            Thread.currentThread().interrupt();
-        }
     }
 }
