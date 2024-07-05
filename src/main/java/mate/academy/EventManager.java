7029f8fb2ae9d51993df0daa576126109b811968
artem <artemsa223@gmail.com>
2024-07-05 19:09:47 +0100

    solution

--- src/main/java/mate/academy/EventManager.java
+++ src/main/java/mate/academy/EventManager.java
@@ -1,19 +1,29 @@
 package mate.academy;
 
-public class EventManager {
-    public void registerListener(EventListener listener) {
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
+public class EventManager {
+    private final Map<Integer, EventListener> listeners = new ConcurrentHashMap<>();
+    private final ExecutorService executorService = Executors.newSingleThreadExecutor();
+
+    public void registerListener(EventListener listener) {
+        listeners.put(listener.hashCode(), listener);
     }
 
     public void deregisterListener(EventListener listener) {
-
+        listeners.remove(listener.hashCode());
     }
 
     public void notifyEvent(Event event) {
-
+        for (EventListener listener : listeners.values()) {
+            executorService.submit(() -> listener.onEvent(event));
+        }
     }
 
     public void shutdown() {
-
+        executorService.shutdown();
     }
 }
