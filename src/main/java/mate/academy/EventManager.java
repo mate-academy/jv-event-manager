9839eee2850751cf7ad83228abd221c27189acc3
Nikita Boyko <nikitabojko57@gmail.com>
2024-08-02 13:45:30 +0300

    implement methods of EventManager.java class

--- src/main/java/mate/academy/EventManager.java
+++ src/main/java/mate/academy/EventManager.java
@@ -1,16 +1,23 @@
 package mate.academy;
 
-public class EventManager {
-    public void registerListener(EventListener listener) {
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
+public class EventManager {
+    private final List<EventListener> listenerList = new CopyOnWriteArrayList<>();
+
+    public void registerListener(EventListener listener) {
+        listenerList.add(listener);
     }
 
     public void deregisterListener(EventListener listener) {
-
+        listenerList.remove(listener);
     }
 
     public void notifyEvent(Event event) {
-
+        for (EventListener eventListener : listenerList) {
+            eventListener.onEvent(event);
+        }
     }
 
     public void shutdown() {
