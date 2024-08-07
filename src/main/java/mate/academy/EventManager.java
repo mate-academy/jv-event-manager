ccd4056a1a5e0f5d36f78075aa51dbc65a24f153
Anatolii Nazarov <anatoly_n@ukr.net>
2024-08-07 10:21:08 +0300

    For check

--- src/main/java/mate/academy/EventManager.java
+++ src/main/java/mate/academy/EventManager.java
@@ -1,19 +1,28 @@
 package mate.academy;
 
-public class EventManager {
-    public void registerListener(EventListener listener) {
+import java.util.ArrayList;
+import java.util.List;
 
+public class EventManager {
+    private List<EventListener> listeners = new ArrayList<>();
+
+    public void registerListener(EventListener listener) {
+        if (listener != null && !listeners.contains(listener)) {
+            listeners.add(listener);
+        }
     }
 
     public void deregisterListener(EventListener listener) {
-
+        listeners.remove(listener);
     }
 
     public void notifyEvent(Event event) {
-
+        for (EventListener listener : listeners) {
+            listener.onEvent(event);
+        }
     }
 
     public void shutdown() {
-
+        listeners.clear();
     }
 }
