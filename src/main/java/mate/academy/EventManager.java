f2c55225a2d2425084188e419f31574e4cef519a
Oleksii Kolinko <138433107+OleksiiKolinko@users.noreply.github.com>
2024-08-23 13:32:18 +0200

    HW64-solution

--- src/main/java/mate/academy/EventManager.java
+++ src/main/java/mate/academy/EventManager.java
@@ -1,19 +1,35 @@
 package mate.academy;
 
-public class EventManager {
-    public void registerListener(EventListener listener) {
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
+public class EventManager {
+    private static final List<EventListener> EVENT_LISTENERS = new CopyOnWriteArrayList<>();
+
+    public void registerListener(EventListener listener) {
+        Thread registerListener = new Thread(() -> EVENT_LISTENERS.add(listener));
+        registerListener.start();
+        try {
+            registerListener.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException("Can't join registerListener", e);
+        }
     }
 
     public void deregisterListener(EventListener listener) {
-
+        Thread deregisterListener = new Thread(() -> EVENT_LISTENERS.remove(listener));
+        deregisterListener.start();
+        try {
+            deregisterListener.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException("Can't join deregisterListener", e);
+        }
     }
 
     public void notifyEvent(Event event) {
-
+        EVENT_LISTENERS.forEach(l -> l.onEvent(event));
     }
 
     public void shutdown() {
-
     }
 }
