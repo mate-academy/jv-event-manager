76f936b3ba4f87f0720c868f3c99e5a7e5ba3714
Artem Klishch <artemklishch@gmail.com>
2024-10-28 18:29:58 +0200

    feat: concurrency

--- src/main/java/mate/academy/EventManager.java
+++ src/main/java/mate/academy/EventManager.java
@@ -1,19 +1,35 @@
 package mate.academy;
 
-public class EventManager {
-    public void registerListener(EventListener listener) {
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
+public class EventManager {
+    private ConcurrentHashMap events = new ConcurrentHashMap();
+
+    private CopyOnWriteArrayList listeners = new CopyOnWriteArrayList();
+
+    private final ExecutorService executorService = Executors.newFixedThreadPool(10);
+
+    public void registerListener(EventListener listener) {
+        listeners.add(listener);
     }
 
     public void deregisterListener(EventListener listener) {
-
+        listeners.remove(listener);
     }
 
     public void notifyEvent(Event event) {
-
+        events.put(event.source(), event);
+        if (listeners.size() > 0) {
+            for (Object listener : listeners) {
+                ((EventListener) listener).onEvent(event);
+            }
+        }
     }
 
     public void shutdown() {
-
+        executorService.shutdown();
     }
 }
