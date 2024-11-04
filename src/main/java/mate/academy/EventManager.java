d5fea2bb457c78d950722f3c4a676cbe73d0b17d
greqit <greqit@gmail.com>
2024-11-04 11:51:21 +0100

    job is done

--- src/main/java/mate/academy/EventManager.java
+++ src/main/java/mate/academy/EventManager.java
@@ -1,19 +1,29 @@
 package mate.academy;
 
-public class EventManager {
-    public void registerListener(EventListener listener) {
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
+public class EventManager {
+    private final List<EventListener> eventListeners = new CopyOnWriteArrayList<>();
+    private final ExecutorService executorService = Executors.newFixedThreadPool(4);
+
+    public void registerListener(EventListener listener) {
+        eventListeners.add(listener);
     }
 
     public void deregisterListener(EventListener listener) {
-
+        eventListeners.remove(listener);
     }
 
     public void notifyEvent(Event event) {
-
+        for (EventListener listener : eventListeners) {
+            executorService.submit(() -> listener.onEvent(event));
+        }
     }
 
     public void shutdown() {
-
+        executorService.shutdown();
     }
 }
