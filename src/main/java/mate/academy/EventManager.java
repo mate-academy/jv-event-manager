64d24b702ae69f697efdcc2dfc05cf59cee40e26
D.Getmansky <d.getmansky@developmentaid.org>
2024-11-17 13:56:27 +0200

    done task

--- src/main/java/mate/academy/EventManager.java
+++ src/main/java/mate/academy/EventManager.java
@@ -1,19 +1,25 @@
 package mate.academy;
 
-public class EventManager {
-    public void registerListener(EventListener listener) {
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
+public class EventManager {
+
+    private final ConcurrentMap<Integer, EventListener> collection = new ConcurrentHashMap<>();
+
+    public void registerListener(EventListener listener) {
+        collection.put(listener.hashCode(), listener);
     }
 
     public void deregisterListener(EventListener listener) {
-
+        collection.remove(listener.hashCode());
     }
 
     public void notifyEvent(Event event) {
-
+        collection.values().forEach(listener -> listener.onEvent(event));
     }
 
     public void shutdown() {
-
+        collection.clear();
     }
 }
