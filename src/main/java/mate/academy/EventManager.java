5fef5b2bb5488d50e65cb3830f15efb1be02a151
Ihor Panchenko <ihpanchenko@gmail.com>
2024-11-18 02:52:51 +0200

    implemented event manager

--- src/main/java/mate/academy/EventManager.java
+++ src/main/java/mate/academy/EventManager.java
@@ -1,19 +1,25 @@
 package mate.academy;
 
-public class EventManager {
-    public void registerListener(EventListener listener) {
+import java.util.concurrent.ConcurrentHashMap;
 
+public class EventManager {
+    private final ConcurrentHashMap<Integer, EventListener> listeners = new ConcurrentHashMap<>();
+
+    public void registerListener(EventListener listener) {
+        listeners.put(listener.hashCode(), listener);
     }
 
     public void deregisterListener(EventListener listener) {
-
+        listeners.remove(listener.hashCode());
     }
 
     public void notifyEvent(Event event) {
-
+        for (EventListener listener : listeners.values()) {
+            listener.onEvent(event);
+        }
     }
 
     public void shutdown() {
-
+        listeners.clear();
     }
 }
