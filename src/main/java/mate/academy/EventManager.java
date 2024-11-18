9a6d93a7e2a36b34c0a88f89cf31c60f02db3163
Ihor Panchenko <ihpanchenko@gmail.com>
2024-11-18 03:02:18 +0200

    implemented event manager

--- src/main/java/mate/academy/EventManager.java
+++ src/main/java/mate/academy/EventManager.java
@@ -1,6 +1,11 @@
 package mate.academy;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 public class EventManager {
     private final ConcurrentHashMap<Integer, EventListener> listeners = new ConcurrentHashMap<>();
@@ -14,8 +19,16 @@ public class EventManager {
     }
 
     public void notifyEvent(Event event) {
+        List<Future> futures = new ArrayList<>();
         for (EventListener listener : listeners.values()) {
-            listener.onEvent(event);
+            futures.add(CompletableFuture.runAsync(() -> listener.onEvent(event)));
+        }
+        try {
+            for (Future future : futures) {
+                future.get();
+            }
+        } catch (InterruptedException | ExecutionException e) {
+            throw new RuntimeException(e);
         }
     }
 
