3e4d4fb470f2a7048db2ad0de535a06cda32c736
OD <sdenisenko055@gmail.com>
2025-01-09 22:07:09 +0200

    result

--- src/main/java/mate/academy/EventManager.java
+++ src/main/java/mate/academy/EventManager.java
@@ -1,19 +1,29 @@
 package mate.academy;
 
-public class EventManager {
-    public void registerListener(EventListener listener) {
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
+public class EventManager {
+    private ExecutorService executorService = Executors.newFixedThreadPool(4);
+    private List<EventListener> eventList = new CopyOnWriteArrayList<>();
+
+    public void registerListener(EventListener listener) {
+        eventList.add(listener);
     }
 
     public void deregisterListener(EventListener listener) {
-
+        eventList.remove(listener);
     }
 
     public void notifyEvent(Event event) {
-
+        for (EventListener eventListener : eventList) {
+            eventListener.onEvent(event);
+        }
     }
 
     public void shutdown() {
-
+        executorService.shutdown();
     }
 }
