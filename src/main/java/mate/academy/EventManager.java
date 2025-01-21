d38fc992f7703f11ab6602d2a221664760b159c7
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2025-01-21 13:34:31 +0200

    Implemented logic of EventManager methods

--- src/main/java/mate/academy/EventManager.java
+++ src/main/java/mate/academy/EventManager.java
@@ -1,19 +1,43 @@
 package mate.academy;
 
-public class EventManager {
-    public void registerListener(EventListener listener) {
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
+public class EventManager {
+    private final List<EventListener> listeners = new CopyOnWriteArrayList<>();
+    private final ExecutorService executor = Executors.newCachedThreadPool();
+
+    public void registerListener(EventListener listener) {
+        listeners.add(listener);
     }
 
     public void deregisterListener(EventListener listener) {
-
+        listeners.remove(listener);
     }
 
     public void notifyEvent(Event event) {
-
+        for (EventListener listener : listeners) {
+            executor.submit(() -> listener.onEvent(event));
+        }
     }
 
     public void shutdown() {
-
+        try {
+            executor.shutdown();
+            if (!executor.awaitTermination(60, TimeUnit.SECONDS)) {
+                executor.shutdownNow();
+                if (!executor.awaitTermination(60, TimeUnit.SECONDS)) {
+                    System.err.println("Executor did not terminate");
+                }
+            }
+        } catch (InterruptedException ie) {
+            executor.shutdownNow();
+            Thread.currentThread().interrupt();
+        } finally {
+            listeners.clear();
+        }
     }
 }
