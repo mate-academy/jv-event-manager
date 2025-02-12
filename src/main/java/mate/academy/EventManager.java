a97702e16339b1ca3599ede96bc704fa6d664547
loranazarenko <loranazarenko@ukr.net>
2025-02-12 18:59:39 +0200

    First Commit

--- src/main/java/mate/academy/EventManager.java
+++ src/main/java/mate/academy/EventManager.java
@@ -1,19 +1,26 @@
 package mate.academy;
 
-public class EventManager {
-    public void registerListener(EventListener listener) {
+import java.util.concurrent.CopyOnWriteArraySet;
 
+public class EventManager {
+    private CopyOnWriteArraySet setOfSubs = new CopyOnWriteArraySet();
+
+    public void registerListener(EventListener listener) {
+        setOfSubs.add(listener);
     }
 
     public void deregisterListener(EventListener listener) {
-
+        setOfSubs.remove(listener);
     }
 
     public void notifyEvent(Event event) {
-
+        for (Object setOfSub : setOfSubs) {
+            EventListener listener = (EventListener) setOfSub;
+            listener.onEvent(event);
+        }
     }
 
     public void shutdown() {
-
+        setOfSubs.clear();
     }
 }
