8fb477f432e910eb4783fe2b9fbcd79ab2735fff
Igor Neminuschii <igorgorslav@gmail.com>
2025-04-01 12:13:10 +0300

    implemented a thread-safe EventManager class

--- src/main/java/mate/academy/EventManager.java
+++ src/main/java/mate/academy/EventManager.java
@@ -1,19 +1,33 @@
 package mate.academy;
 
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.LinkedBlockingQueue;
+
 public class EventManager {
+    private final BlockingQueue<EventListener> queue;
+    private final ExecutorService executor;
+
+    public EventManager() {
+        queue = new LinkedBlockingQueue<>();
+        executor = ForkJoinPool.commonPool();
+    }
+
     public void registerListener(EventListener listener) {
+        queue.add(listener);
 
     }
 
     public void deregisterListener(EventListener listener) {
-
+        queue.remove(listener);
     }
 
     public void notifyEvent(Event event) {
-
+        queue.forEach(eventListener -> eventListener.onEvent(event));
     }
 
     public void shutdown() {
-
+        executor.shutdown();
     }
 }
